a37de815698383e0bb5c8f8d6adc131e31639ef7
Nick Telenson <ntelenson@gmail.com>
2019-08-21 19:24:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 p load_library('emoticons.yml')
 
 def get_japanese_emoticon(file_path, eng_emot)
-  load_library(file_path)
+  load_library(file_path)[:get_emoticon][eng_emot]
 end
 
 def get_english_meaning
