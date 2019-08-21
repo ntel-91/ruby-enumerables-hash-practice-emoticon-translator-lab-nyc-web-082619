e84f223275755f5f8dca2d482337b60e2006622e
Nick Telenson <ntelenson@gmail.com>
2019-08-21 18:44:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,25 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  require "yaml"
+  lib = YAML.load_file(file_path)
+  
+  emot_lib = {
+    "get_meaning": {},
+    "get_emoticon": {}
+  }
+      
+  emot_lib.each do |key, value|
+    key[key.keys[0]] = {
+      value.keys[0] => value[value.keys[0]][1]
+    }
+  end 
+
+#  emot_lib[emot_lib.keys[0]] = {lib.keys[0] => lib[lib.keys[0]][1]}
+  emot_lib
 end
+
+p load_library('emoticons.yml')
 
 def get_japanese_emoticon
   # code goes here
---
+++ notes.rb
@@ -0,0 +1,26 @@
+  
+require "yaml"
+lib = YAML.load_file('./lib/emoticons.yml')
+  
+emot_lib = {
+    "get_meaning": {},
+    "get_emoticon": {}
+  }
+
+emot_lib[:get_meaning] = {lib.keys[0] => lib.values[0][1]}
+#p emot_lib[:get_meaning] == {}
+#p emot_lib.keys[0] == :get_meaning
+#p lib.keys[0]
+#p lib.values[0][1]
+
+
+# note: key == lib.keys[0] and value[0] = lib.values[0][1]
+#lib.each do |key, value|
+#  emot_lib.keys[0] = {key => value[0]}
+#end
+
+p emot_lib
+
+
+
+
