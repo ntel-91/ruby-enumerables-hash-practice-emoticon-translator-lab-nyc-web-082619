4dcc0597a211781f999f3eefd863922695a58055
Nick Telenson <ntelenson@gmail.com>
2019-08-21 19:08:04 +0000

    Automatically backed up by Learn

--- notes.rb
+++ notes.rb
@@ -16,7 +16,7 @@ emot_lib[:get_meaning] = {lib.keys[0] => lib.values[0][1]}
 
 # note: key == lib.keys[0] and value[0] = lib.values[0][1]
 lib.each do |key, value|
-  emot_lib[emot_lib.keys[0]] = {key => value[0]}
+  p emot_lib[emot_lib.keys[0]] = {key => value[0]}
 end
 
 
